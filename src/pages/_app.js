b86b7b2953741b65018527ee17df2acebca3976e
BMB <braynmbarbosa@gmail.com>
2025-02-24 21:15:47 +0000

    upgrade _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 //import '../styles/globals.css'; // Ensure global styles are imported here
-import '../styles/Exodo.css';
 import '../styles/Genesis.css';
+import '../styles/Exodo.css';
 import '../styles/Marcos.css';
 import '../styles/Matheus.css';
 
