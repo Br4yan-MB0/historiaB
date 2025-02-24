aa7f84147f81ebafc1f7c6bd91eaec44ef501595
BMB <braynmbarbosa@gmail.com>
2025-02-24 21:17:56 +0000

    upgrade _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
 //import '../styles/globals.css'; // Ensure global styles are imported here
-import '../styles/Genesis.css';
-import '../styles/Exodo.css';
-import '../styles/Marcos.css';
-import '../styles/Matheus.css';
+import '../styles/genesis.css';
+import '../styles/exodo.css';
+import '../styles/marcos.css';
+import '../styles/matheus.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
