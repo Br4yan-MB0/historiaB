92df7e6f4688b3d0f98ea49bf307ba78a5bb7066
BMB <braynmbarbosa@gmail.com>
2025-02-27 07:52:46 +0000

    finalizing upgrade myproject

---
+++ src/pages/velho-testamento/1Samuel.js
@@ -0,0 +1,13 @@
+import Layout from '../../components/Layout';
+//import '../../styles/genesis.css';
+
+export default function Genesis() {
+  return (
+    <Layout>
+      <div className="container">
+        <h1>📖 Gênesis</h1>
+        <p>No princípio, Deus criou os céus e a terra...</p>
+      </div>
+    </Layout>
+  );
+}
