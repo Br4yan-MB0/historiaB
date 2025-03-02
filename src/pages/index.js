b1686b21410943bd40240ec05d852baa9351e462
BMB <braynmbarbosa@gmail.com>
2025-03-02 20:04:02 +0000

    agora vai

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import styles from '../styles/home.module.css';  // Importando o CSS
 
 export default function Home() {
   return (
@@ -6,26 +7,86 @@ export default function Home() {
       <h1>Histórias Bíblicas Infantis</h1>
       
       <h2>Velho Testamento</h2>
-      <ul>
-        <li><Link href="/velho-testamento/Genesis">Gênesis</Link></li>
-        <li><Link href="/velho-testamento/Exodo">Êxodo</Link></li>
-        <li><Link href="/velho-testamento/Juizes">Juízes</Link></li>
-        <li><Link href="/velho-testamento/UmSamuel">1º Samuel</Link></li>
-        <li><Link href="/velho-testamento/DoisReis">2º Reis</Link></li>
-        <li><Link href="/velho-testamento/Ester">Ester</Link></li>
-        <li><Link href="/velho-testamento/Jo">Jó</Link></li>
-        <li><Link href="/velho-testamento/Salmos">Salmos</Link></li>
-        <li><Link href="/velho-testamento/Proverbios">Provérbios</Link></li>
-        <li><Link href="/velho-testamento/Daniel">Daniel</Link></li>
-        <li><Link href="/velho-testamento/Jonas">Jonas</Link></li>
+      <ul className={styles.listContainer}>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Genesis">
+            <button className={styles.btn}>Gênesis</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Exodo">
+            <button className={styles.btn}>Êxodo</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Juizes">
+            <button className={styles.btn}>Juízes</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/UmSamuel">
+            <button className={styles.btn}>1º Samuel</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/DoisReis">
+            <button className={styles.btn}>2º Reis</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Ester">
+            <button className={styles.btn}>Ester</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Jo">
+            <button className={styles.btn}>Jó</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Salmos">
+            <button className={styles.btn}>Salmos</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Proverbios">
+            <button className={styles.btn}>Provérbios</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Daniel">
+            <button className={styles.btn}>Daniel</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/velho-testamento/Jonas">
+            <button className={styles.btn}>Jonas</button>
+          </Link>
+        </li>
       </ul>
 
       <h2>Novo Testamento</h2>
-      <ul>
-        <li><Link href="/novo-testamento/matheus">Mateus</Link></li>
-        <li><Link href="/novo-testamento/marcos">Marcos</Link></li>
-        <li><Link href="/novo-testamento/lucas">Lucas</Link></li>
-        <li><Link href="/novo-testamento/joao">João</Link></li>
+      <ul className={styles.listContainer}>
+        <li className={styles.listItem}>
+          <Link href="/novo-testamento/matheus">
+            <button className={styles.btn}>Mateus</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/novo-testamento/marcos">
+            <button className={styles.btn}>Marcos</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/novo-testamento/lucas">
+            <button className={styles.btn}>Lucas</button>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/novo-testamento/joao">
+            <button className={styles.btn}>João</button>
+          </Link>
+        </li>
       </ul>
     </div>
   );
